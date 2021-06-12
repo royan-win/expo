d4b4e853f75b6946427ce4e23eedb39981acde92
Hardik Sakpal <40515075+scratch-coder-2514@users.noreply.github.com>
2021-06-12 10:25:09 +0530

    updated css register file

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,26 +1,53 @@
+import React from 'react';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 function Register() {
+    
     return (
-        <div className="flex text-white bg-gray-900 items-center justify-items-center">
-            <div className="ml-auto p-12 sm:p-12 mb-32 bg-gray-800 mr-12 sm:mr-24 mt-10">
+        <div className="flex p-5 text-white bg-back_img items-center justify-items-center">
+            <div className="relative z-0 w-96 ml-40 mb-40">
+                <Carousel
+                autoPlay
+                infiniteLoop
+                showStatus={false}
+                showIndicators={false}
+                showThumbs={false}
+                interval={5000}
+                >
+                    <div>
+                        <img className="rounded-full w-32 h-96" loading="lazy" src="https://ymvirtual.s3.amazonaws.com/media/box-03_SmMKZar.jpg" alt=""/>
+                    </div>
+
+                    <div>
+                        <img className="rounded-full w-32 h-96" loading="lazy" src="https://ymvirtual.s3.amazonaws.com/media/box-02_Ybl88WU.jpg" alt=""/>
+                    </div>
+
+                    <div>
+                        <img className="rounded-full w-32 h-96" loading="lazy" src="https://ymvirtual.s3.amazonaws.com/media/box-04_RBWX32s.jpg" alt=""/>
+                    </div>
+                </Carousel>
+            </div>
+            <div className="ml-auto p-8 pl-24 pr-28 mb-32 bg-gray-800 bg-opacity-70 mr-12 sm:mr-16 mt-7">
                 <div className="ml-6">
                     <h1 className="text-3xl font-bold text-blue-500 pb-2">#EXPO 2021</h1>
                     <h1 className="text-2xl pl-4 text-blue-400 font-semibold">Register Now</h1>
                 </div>
                 <div className="p-4 lg:w-80 sm:w-28">
                     <h1 className="text-lg pb-1">Name:</h1>
-                    <input className="bg-transparent outline-none p-2" type="text" placeholder="Enter Full Name"/>
+                    <input className="bg-gray-700 bg-opacity-40 rounded-xl w-80 outline-none p-2 m-1" type="text" placeholder="Enter Full Name"/>
 
                     <h1 className="text-lg pb-1">Mobile Number:</h1>
-                    <input className="outline-none bg-transparent p-2" type="text" placeholder="Enter Mobile Number"/>
+                    <input className="outline-none bg-gray-700 bg-opacity-40 rounded-xl w-80 p-2 m-1" type="text" placeholder="Enter Mobile Number"/>
 
                     <h1 className="text-lg pb-1">Email:</h1>
-                    <input className="outline-none bg-transparent p-2" type="text" placeholder="Enter Email"/>
+                    <input className="outline-none bg-gray-700 bg-opacity-40 rounded-xl w-80 p-2 m-1" type="text" placeholder="Enter Email"/>
 
                     <h1 className="text-lg pb-1">Company Name:</h1>
-                    <input className="outline-none bg-transparent p-2" type="text" placeholder="Enter Company Name"/>
+                    <input className="outline-none bg-gray-700 bg-opacity-40 rounded-xl w-80 p-2 m-1" type="text" placeholder="Enter Company Name"/>
 
                     <h1 className="text-lg pb-1">Designation:</h1>
-                    <input className="outline-none bg-transparent p-2" type="text" placeholder="Enter Designation"/>
+                    <input className="outline-none bg-gray-700 bg-opacity-40 rounded-xl w-80 p-2 m-1" type="text" placeholder="Enter Designation"/>
                 </div>
                 <div className="inline-block">
                     <button className="p-3 ml-16 bg-blue-600 rounded-2xl">Register</button>
